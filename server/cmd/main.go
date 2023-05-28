fc23c53d0b3f07e7350ac1e043292b352e6d6caa
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-05-28 16:08:51 +0700

    [add] berhasil menambahkan sccop,migrate,dan membuat table users

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,12 +3,11 @@ package main
 import(
 	"log"
 	"server/DB"
-
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 )
 func main() {
-	_, err := db.NewDatabase()
+	_, err := DB.NewDatabase()
 	if err != nil {
 		log.Fatal("could not initialize database connection: %s", err)
 	}
