dd6dffedd0c43818adb030de46fe479ca98ed088
Qii <57295427+syauqqii@users.noreply.github.com>
2023-06-07 00:48:42 +0700

    Update main.go

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"server/db"
+	"server/internal/ws"
 	"server/internal/user"
 	"server/router"
 	"server/utils"
@@ -25,6 +26,14 @@ func main() {
 	port := os.Getenv("APP_PORT")
 
 	fmt.Printf("\n > Server running on: %s", utils.Serv.Sprintf("http://%s:%s", host, port))
+	fmt.Printf("\n\n > Route API:")
+	fmt.Printf("\n   - /signup")
+	fmt.Printf("\n   - /login")
+	fmt.Printf("\n   - /logout")
+	fmt.Printf("\n   - /ws/create_room")
+	fmt.Printf("\n   - /ws/join_room/{:roomId}")
+	fmt.Printf("\n   - /ws/get_rooms")
+	fmt.Printf("\n   - /ws/get_clients/{:roomId}")
 	fmt.Println("\n\n -------------------- [ LOG History ] --------------------\n")
 
 	if dbug == "1" {
@@ -41,8 +50,12 @@ func main() {
 	userSvc := user.NewService(userRep)
 	userHandler := user.NewHandler(userSvc)
 
+	hub := ws.NewHub()
+	wsHandler := ws.NewHandler(hub)
+	go hub.Run()
+
 	address := fmt.Sprintf("%s:%s", host, port)
 
-	router.InitRouter(userHandler)
+	router.InitRouter(userHandler, wsHandler)
 	router.Start(address)
 }
